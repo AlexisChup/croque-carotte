9c0a33246793fdb0b578eef6dd2308a5898a4032
alexischup <alexischup@gmail.com>
2020-10-23 16:59:56 +0200

    Merge 2

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ if __name__ == "__main__":
     currentCard = MOVING_CARROT
 
     while(isPlaying):
+        # MENU & CURRENT PLAYER
         displayMenu()
         displayCurrentPlayer(currentPlayer)
 
+        # IS PLAYING AGAIN
         displayOptions()
         answer = input()
         isPlaying = False if (answer == 'q') else True
 
+        # BOARD
         displayBoard()
         currentCard = getCard()
         displayValueOfCard(currentCard)
+
+        # MOOVE RABBIT
+        keyOfRabbit = chooseRabbitToMoove(currentPlayer)
+        mooveRabbitOnBoard(keyOfRabbit, 1, currentPlayer)
+        displayBoard()
 
         currentPlayer = (currentPlayer + 1) % 2
         
